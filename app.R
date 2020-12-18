2402911729997392b98af073484125d44307a9f2
chrl3hr5 <chrl3hr5@gmail.com>
2020-12-19 02:12:13 +0530

    Custom maps and sidebar

--- app.R
+++ app.R
@@ -1,10 +1,10 @@
 # Loading libraries
 library(tidyverse) # For data analysis
 library(data.table) # For performance
+library(skimr) # For data summary
 library(COVID19) # For COVID-19 data
 library(shiny) # For R Shiny
 library(shinydashboard) # For Shiny dashboard
-library(semantic.dashboard) # For Semantic dashboard
 library(leaflet) # For maps
 library(styler) # For formatting R code
 
@@ -14,17 +14,26 @@ Data <- as.data.table(covid19()) # Worldwide data by country
 # User Interface
 ui <- dashboardPage(
   dashboardHeader(title = "COVID-19 Tracker"),
-  dashboardSidebar(),
-  dashboardBody(leafletOutput(outputId = "COVID Map"))
+  dashboardSidebar(
+    sidebarMenu(
+      menuItem("Dashboard", tabName = "Dashboard", icon = icon("dashboard")),
+      menuItem("Data Visualization", tabName = "Visual", icon = icon("window-maximize"))
+  )),
+  dashboardBody(
+    tabItems(
+      tabItem(tabName = "Dashboard", leafletOutput(outputId = "COVID Map"),
+              selectInput(inputId = "Map", label = "List of Maps", choices = names(providers), selected = "Stamen.Watercolor"),
+      tabItem(tabName = "Visual")
+  )))
 )
 
 # Server
 server <- function(input, output) {
   output$`COVID Map` <- renderLeaflet({
     leaflet(filter(Data, !is.na(latitude) & !is.na(longitude))) %>%
+      setView(lng = 78, lat = 20, zoom = 3) %>%
       addTiles() %>%
-      fitBounds(~ min(latitude), ~ min(longitude), ~ max(latitude), ~ max(longitude)) %>%
-      addCircleMarkers(stroke = F, opacity = 0.5, radius = 2, popup = "Map")
+      addProviderTiles(input$Map)
   })
 }
 
