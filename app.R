04bc6d753ce2ba6c0a72381d38bb0a1c247f478d
chrl3hr5 <chrl3hr5@gmail.com>
2020-12-27 02:03:44 +0530

    Changes in map with other minor adjustments

--- app.R
+++ app.R
@@ -1,5 +1,6 @@
 # Loading libraries
 library(tidyverse) # For data analysis
+library(plotly) # For data visualization
 library(data.table) # For performance
 library(skimr) # For data summary
 library(COVID19) # For COVID-19 data
@@ -7,10 +8,11 @@ library(shiny) # For R Shiny
 library(semantic.dashboard) # For Semantic UI dashboard
 library(leaflet) # For maps
 library(styler) # For formatting R code
-library(waiter) # Loading screens
+library(waiter) # For loading screens
 
 # Loading data
-Data <- as.data.table(covid19()) # Worldwide data by country
+Data <- covid19() # Worldwide data by country
+colnames(Data) <- gsub("_", " ", str_to_title(colnames(Data))) # Reformatting column names
 
 # User Interface
 ui <- dashboardPage(
@@ -24,11 +26,11 @@ ui <- dashboardPage(
   dashboardBody(
     tabItems(
       tabItem(
-        "Dashboard", box(leafletOutput(outputId = "COVID Map"), selectInput(inputId = "Map", label = "List of Maps", choices = names(providers), selected = "Stamen.Watercolor"), width = 16, solidHeader = TRUE, title = "COVID-19 MAP")
+        "Dashboard", box(leafletOutput(outputId = "COVID Map"), selectInput(inputId = "Information", label = "Select variable", choices = colnames(Data)[!colnames(Data) %in% c("Id", "Iso alpha 3", "Iso alpha 2", "Currency", "Administrative area level", "Administrative area level 1", "Administrative area level 2", "Administrative area level 3", "Latitude", "Longitude", "Key", "Key apple mobility", "Key google mobility")], selected = "Tests"), sliderInput(inputId = "Time", label = "Choose date", min = Data$Date[1], max = Data$Date[length(Data$Date)], timeFormat = "%F", value = Data$Date[1]), width = 16, solidHeader = TRUE, title = "COVID-19 MAP", title_side = "top left", collapsible = F)
       ),
       tabItem(
         "Visual",
-        box(plotOutput(outputId = "Plots"), width = 16)
+        fluidRow(box(plotlyOutput(outputId = "Plots"), width = 16, color = "blue", ribbon = T, title = "Recovery", title_side = "top left", collapsible = F, fluidRow(selectInput(inputId = "Plot x-axis", label = "Select x-axis variable", choices = colnames(Data)[!colnames(Data) %in% c("Id", "Iso alpha 3", "Iso alpha 2", "Currency", "Administrative area level", "Administrative area level 1", "Administrative area level 2", "Administrative area level 3", "Latitude", "Longitude", "Key", "Key apple mobility", "Key google mobility")]), selectInput(inputId = "Plot y-axis", label = "Select y-axis variable", choices = colnames(Data)[!colnames(Data) %in% c("Id", "Date", "Iso alpha 3", "Iso alpha 2", "Currency", "Administrative area level", "Administrative area level 1", "Administrative area level 2", "Administrative area level 3", "Latitude", "Longitude", "Key", "Key apple mobility", "Key google mobility")]), selectInput(inputId = "Plot color", label = "Select color variable", choices = colnames(Data)[colnames(Data) %in% c("Id", "Iso alpha 3", "Iso alpha 2", "Currency", "Administrative area level 1", "Key apple mobility", "Key google mobility")]))))
       )
     )
   )
@@ -37,17 +39,17 @@ ui <- dashboardPage(
 # Server
 server <- function(input, output) {
   output$`COVID Map` <- renderLeaflet({
-    leaflet(filter(Data, !is.na(latitude) & !is.na(longitude))) %>%
-      setView(lng = 78, lat = 20, zoom = 4) %>%
+    leaflet(filter(Data, !is.na(Latitude) & !is.na(Longitude)), options = leafletOptions(minZoom = 2)) %>%
+      setView(lng = 78, lat = 20, zoom = 2) %>%
       addTiles() %>%
-      addProviderTiles(input$Map)
+      addProviderTiles("Stamen.Watercolor")
   })
-  output$Plots <- renderPlot({
-    ggplot(Data) +
-      geom_point(aes(x = population, y = recovered, color = tests)) +
-      labs(x = "Population", y = "Recovered") +
+  output$Plots <- renderPlotly({
+    ggplotly(ggplot(Data) +
+      geom_point(aes(x = eval(parse(text = paste0("`", input$`Plot x-axis`, "`"))), y = eval(parse(text = paste0("`", input$`Plot y-axis`, "`"))), color = eval(parse(text = paste0("`", input$`Plot color`, "`"))))) +
+      labs(x = input$`Plot x-axis`, y = input$`Plot y-axis`) +
       theme_bw() +
-      theme(legend.position = NULL)
+      theme(legend.position = "none"))
   })
 }
 
