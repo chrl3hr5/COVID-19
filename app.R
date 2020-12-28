3335059e31a7f9ae00d882764b6a680191ce0c4d
chrl3hr5 <chrl3hr5@gmail.com>
2020-12-28 06:08:35 +0530

    Changes in plot, slider and dropdown menus

--- app.R
+++ app.R
@@ -24,13 +24,25 @@ ui <- dashboardPage(
     )
   ),
   dashboardBody(
+    tags$head(tags$style(".selectize-dropdown {position: static}")),
     tabItems(
       tabItem(
-        "Dashboard", box(leafletOutput(outputId = "COVID Map"), selectInput(inputId = "Information", label = "Select variable", choices = colnames(Data)[!colnames(Data) %in% c("Id", "Iso alpha 3", "Iso alpha 2", "Currency", "Administrative area level", "Administrative area level 1", "Administrative area level 2", "Administrative area level 3", "Latitude", "Longitude", "Key", "Key apple mobility", "Key google mobility")], selected = "Tests"), sliderInput(inputId = "Time", label = "Choose date", min = Data$Date[1], max = Data$Date[length(Data$Date)], timeFormat = "%F", value = Data$Date[1]), width = 16, solidHeader = TRUE, title = "COVID-19 MAP", title_side = "top left", collapsible = F)
+        "Dashboard", box(leafletOutput(outputId = "COVID Map"),
+          selectInput(inputId = "Information", width = "100%", label = "Select variable", choices = colnames(Data)[!colnames(Data) %in% c("Id", "Iso alpha 3", "Iso alpha 2", "Currency", "Administrative area level", "Administrative area level 1", "Administrative area level 2", "Administrative area level 3", "Latitude", "Longitude", "Key", "Key apple mobility", "Key google mobility")], selected = "Tests"),
+          sliderInput(inputId = "Time", width = "100%", label = "Choose date", min = Data$Date[1], max = Data$Date[length(Data$Date)], timeFormat = "%F", value = Data$Date[1]),
+          width = 16, solidHeader = TRUE, title = "COVID-19 MAP", title_side = "top left", collapsible = F
+        )
       ),
       tabItem(
         "Visual",
-        fluidRow(box(plotlyOutput(outputId = "Plots"), width = 16, color = "blue", ribbon = T, title = "Recovery", title_side = "top left", collapsible = F, fluidRow(selectInput(inputId = "Plot x-axis", label = "Select x-axis variable", choices = colnames(Data)[!colnames(Data) %in% c("Id", "Iso alpha 3", "Iso alpha 2", "Currency", "Administrative area level", "Administrative area level 1", "Administrative area level 2", "Administrative area level 3", "Latitude", "Longitude", "Key", "Key apple mobility", "Key google mobility")]), selectInput(inputId = "Plot y-axis", label = "Select y-axis variable", choices = colnames(Data)[!colnames(Data) %in% c("Id", "Date", "Iso alpha 3", "Iso alpha 2", "Currency", "Administrative area level", "Administrative area level 1", "Administrative area level 2", "Administrative area level 3", "Latitude", "Longitude", "Key", "Key apple mobility", "Key google mobility")]), selectInput(inputId = "Plot color", label = "Select color variable", choices = colnames(Data)[colnames(Data) %in% c("Id", "Iso alpha 3", "Iso alpha 2", "Currency", "Administrative area level 1", "Key apple mobility", "Key google mobility")]))))
+        fluidRow(box(plotlyOutput(outputId = "Plots"),
+          width = 16, color = "blue", ribbon = T, title = "COVID-19 Graph", title_side = "top left", collapsible = F,
+          fluidRow(
+            selectInput(inputId = "Plot x-axis", width = "100%", label = "Select x-axis variable", choices = colnames(Data)[!colnames(Data) %in% c("Id", "Iso alpha 3", "Iso alpha 2", "Currency", "Administrative area level", "Administrative area level 1", "Administrative area level 2", "Administrative area level 3", "Latitude", "Longitude", "Key", "Key apple mobility", "Key google mobility")]),
+            selectInput(inputId = "Plot y-axis", width = "100%", label = "Select y-axis variable", choices = colnames(Data)[!colnames(Data) %in% c("Id", "Date", "Iso alpha 3", "Iso alpha 2", "Currency", "Administrative area level", "Administrative area level 1", "Administrative area level 2", "Administrative area level 3", "Latitude", "Longitude", "Key", "Key apple mobility", "Key google mobility")]),
+            selectInput(inputId = "Plot color", width = "100%", label = "Select color variable", choices = colnames(Data)[colnames(Data) %in% c("Id", "Iso alpha 3", "Iso alpha 2", "Currency", "Administrative area level 1", "Key apple mobility", "Key google mobility")])
+          )
+        ))
       )
     )
   )
@@ -45,11 +57,19 @@ server <- function(input, output) {
       addProviderTiles("Stamen.Watercolor")
   })
   output$Plots <- renderPlotly({
-    ggplotly(ggplot(Data) +
-      geom_point(aes(x = eval(parse(text = paste0("`", input$`Plot x-axis`, "`"))), y = eval(parse(text = paste0("`", input$`Plot y-axis`, "`"))), color = eval(parse(text = paste0("`", input$`Plot color`, "`"))))) +
-      labs(x = input$`Plot x-axis`, y = input$`Plot y-axis`) +
+    plot <- ggplot(Data, aes(x = eval(parse(text = paste0("`", input$`Plot x-axis`, "`"))), y = eval(parse(text = paste0("`", input$`Plot y-axis`, "`"))))) +
+      geom_point(aes(
+        color = eval(parse(text = paste0("`", input$`Plot color`, "`"))),
+        text = paste(
+          input$`Plot x-axis`, ":", eval(parse(text = paste0("`", input$`Plot x-axis`, "`"))), "\n",
+          input$`Plot y-axis`, ":", eval(parse(text = paste0("`", input$`Plot y-axis`, "`"))), "\n",
+          input$`Plot color`, ":", eval(parse(text = paste0("`", input$`Plot color`, "`")))
+        ), stroke = 0
+      )) +
+      labs(x = input$`Plot x-axis`, y = input$`Plot y-axis`, color = input$`Plot color`) +
       theme_bw() +
-      theme(legend.position = "none"))
+      theme(legend.position = "none")
+    ggplotly(plot, tooltip = "text")
   })
 }
 
