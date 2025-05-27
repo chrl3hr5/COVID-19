42c504e1c97b736cf1a4b9eb2f623412ec0804ae
chrl3hr5 <chrl3hr5@gmail.com>
2025-05-27 12:32:38 +0530

    Data processing to create map and visualizations

---
+++ data/data.R
@@ -0,0 +1,48 @@
+# Check for the presence of polygon data file and extract data from web if file is unavailable
+if(file.exists('data/polygons.rds')) {
+  polygons <- readRDS('data/polygons.rds')
+} else {
+  # API to obtain up-to-date boundaries (Source: https://github.com/wmgeolab/geoBoundaries)
+  url <- "https://api.github.com/repos/wmgeolab/geoBoundaries/contents/sourceData/gbOpen"
+  
+  # Obtaining country codes
+  country_codes <- unique(substr(sapply(fromJSON(content(GET(url), "text"))$name, as.character), 1, 3))
+  
+  # Obtaining polygons for world countries
+  polygons <- data.frame()
+  adm_level <- "ADM0"
+  for(i in 1:length(country_codes)) {
+    iso3 <- country_codes[i]
+    try(polygons <- rbind(polygons,st_read(fromJSON(content(GET(paste0("https://www.geoboundaries.org/api/current/gbOpen/", iso3, "/", adm_level, "/")), "text"))$simplifiedGeometryGeoJSON)))
+    Sys.sleep(2) # 2 seconds delay between requests
+  }
+  
+  # Saving data
+  saveRDS(polygons,'data/polygons.rds')
+}
+
+# Getting COVID-19 data
+data <- covid19(polygons$shapeName,verbose = FALSE) # Global COVID-19 data by country
+
+# Converting cumulative values to absolute (Source: https://covid19datahub.io/articles/docs.html)
+cumulative_to_individual <- function(x)
+{
+  if(length(x) > 1)
+  {
+    abs(x[2:length(x)] - x[1:(length(x) - 1)])
+  } else {
+    cat("Vector length not sufficient!\n")
+  }
+}
+manipulated_data <- rbind(data[1, c(3:9, 11:21)], apply(data[, c(3:9, 11:21)], 2, cumulative_to_individual))
+manipulated_data <- cbind(data[, c(1:2)], manipulated_data, data[, c(10, 22:ncol(data))])
+data <- cbind(1:nrow(data),data)
+colnames(data)[1] <- "index"
+grouped_data <- data %>% group_split(id)
+replace_values <- lapply(grouped_data,function(x){head(x,1)})
+replace_values <- do.call(rbind,replace_values)
+replace_values <- replace_values[order(replace_values$index),]
+manipulated_data[replace_values$index,] <- data[replace_values$index,-1]
+
+# Removing objects that are not required
+rm(grouped_data,replace_values,cumulative_to_individual)
