c10359ac47a0ea36de53d67219b54cef3195d1db
chrl3hr5 <chrl3hr5@gmail.com>
2025-05-27 17:45:03 +0530

    Changed variable names in 'server' and font in 'ui'

--- app.R
+++ app.R
@@ -22,28 +22,29 @@ ui <- dashboardPage(
   dashboardBody(
     Theme,
     tags$head(tags$style(".selectize-dropdown {position: static}")),
-    tags$head(tags$style(HTML(".selectize-input {white-space: nowrap} 
+    tags$head(tags$style(HTML(".selectize-input {white-space: nowrap}
+                              body {font-family: 'Helvetica Neue', Helvetica, Arial, sans-serif;}
                               #information+ div>.selectize-input{color: black;}
                               #select_country+ div>.selectize-input{color: black;}
                               #plot_x_axis+ div>.selectize-input{color: black;}
-                              #Plot_y-axis+ div>.selectize-input{color: black;}"))),
+                              #plot_y_axis+ div>.selectize-input{color: black;}"))),
     tags$style(type = "text/css", ".irs-grid-text {color: black; bottom: 5px; z-index: 1;}"),
     tabItems(
       tabItem(
         "Dashboard", box(withSpinner(leafletOutput(outputId = "map")), br(),
-          fluidRow(box(width = 12, solidHeader = TRUE, title = span(icon("laptop-code"), "SELECT VARIABLE"), status = "primary", selectInput(inputId = "information", width = "100%", label = NULL, choices = colnames(manipulated_data)[!colnames(manipulated_data) %in% c("Id", "Date", "Administrative area level 1")], selected = "Tests"))),
-          fluidRow(box(width = 12, solidHeader = TRUE, title = span(icon("calendar-alt"), "CHOOSE TIME PERIOD"), status = "primary", div(style = "margin: auto; width: 95%", sliderInput(inputId = "time", width = "100%", label = NULL, min = manipulated_data$date[1], max = manipulated_data$date[length(manipulated_data$date)], timeFormat = "%F", value = manipulated_data$date[1])))),
-          width = 16, solidHeader = TRUE, title = span(icon("globe"), "COVID-19 MAP"), title_side = "top left", collapsible = F, status = "primary"
+          fluidRow(box(width = 12, solidHeader = TRUE, title = span(icon("laptop-code"), "SELECT VARIABLE", style = "font-weight: bold; font-family: 'Helvetica Neue', Helvetica, Arial, sans-serif;"), status = "primary", selectInput(inputId = "information", width = "100%", label = NULL, choices = colnames(manipulated_data)[!colnames(manipulated_data) %in% c("Id", "Date", "Administrative Area Level 1")], selected = "Confirmed"))),
+          fluidRow(box(width = 12, solidHeader = TRUE, title = span(icon("calendar-alt"), "CHOOSE TIME PERIOD", style = "font-weight: bold; font-family: 'Helvetica Neue', Helvetica, Arial, sans-serif;"), status = "primary", div(style = "margin: auto; width: 95%", sliderInput(inputId = "time", width = "100%", label = NULL, min = manipulated_data$Date[1], max = manipulated_data$Date[length(manipulated_data$Date)], timeFormat = "%F", value = manipulated_data$Date[1])))),
+          width = 16, solidHeader = TRUE, title = span(icon("globe"), "COVID-19 MAP", style = "font-weight: bold; font-family: 'Helvetica Neue', Helvetica, Arial, sans-serif;"), title_side = "top left", collapsible = F, status = "primary"
         )
       ),
       tabItem(
         "Visual",
-        box(plotlyOutput(outputId = "Plots", height = "325px"), br(),
+        box(withSpinner(plotlyOutput(outputId = "Plots", height = "325px")), br(),
           width = 16, color = "blue", title = span(icon("chart-line"), "COVID-19 GRAPH"), title_side = "top left", collapsible = F, status = "primary", solidHeader = T,
           fluidRow(
-            column(4, box(width = 12, solidHeader = TRUE, title = span(icon("flag"), "SELECT COUNTRY"), status = "primary", selectInput(inputId = "select_country", width = "100%", label = NULL, choices = unique(manipulated_data$administrative_area_level_1), selected = "India"))),
-            column(4, box(width = 12, solidHeader = TRUE, title = "SELECT X-AXIS VARIABLE", status = "primary", selectInput(inputId = "plot_x_axis", width = "100%", label = NULL, choices = colnames(manipulated_data)[!colnames(manipulated_data) %in% c("Id", "Administrative area level 1")], selected = "Date"))),
-            column(4, box(width = 12, solidHeader = TRUE, title = "SELECT Y-AXIS VARIABLE", status = "primary", selectInput(inputId = "plot_y_axis", width = "100%", label = NULL, choices = colnames(manipulated_data)[!colnames(manipulated_data) %in% c("Id", "Date", "Administrative area level 1")], selected = "Tests")))
+            column(4, box(width = 12, solidHeader = TRUE, title = span(icon("flag"), "SELECT COUNTRY", style = "font-weight: bold; font-family: 'Helvetica Neue', Helvetica, Arial, sans-serif;"), status = "primary", selectInput(inputId = "select_country", width = "100%", label = NULL, choices = unique(manipulated_data$`Administrative Area Level 1`), selected = "India"))),
+            column(4, box(width = 12, solidHeader = TRUE, title = span("SELECT X-AXIS VARIABLE", style = "font-weight: bold; font-family: 'Helvetica Neue', Helvetica, Arial, sans-serif;"), status = "primary", selectInput(inputId = "plot_x_axis", width = "100%", label = NULL, choices = colnames(manipulated_data)[!colnames(manipulated_data) %in% c("Id", "Administrative Area Level 1")], selected = "Date"))),
+            column(4, box(width = 12, solidHeader = TRUE, title = span("SELECT Y-AXIS VARIABLE", style = "font-weight: bold; font-family: 'Helvetica Neue', Helvetica, Arial, sans-serif;"), status = "primary", selectInput(inputId = "plot_y_axis", width = "100%", label = NULL, choices = colnames(manipulated_data)[!colnames(manipulated_data) %in% c("Id", "Date", "Administrative Area Level 1")], selected = "Confirmed")))
           )
         )
       )
@@ -55,9 +56,9 @@ ui <- dashboardPage(
 server <- function(input, output) {
   # Obtaining data to display on map based on the selected variable and time period
   map_data <- reactive({req(input$time, input$information)
-    manipulated_data %>% filter(date == input$time) %>% mutate(
+    manipulated_data %>% filter(Date == input$time) %>% mutate(
       map_value = as.numeric(.data[[input$information]]),
-      shapeName = administrative_area_level_1, .keep = "none")
+      shapeName = `Administrative Area Level 1`, .keep = "none")
   })
   # Merging the obtained data with polygon data
   map_data_merged <- reactive({
@@ -74,11 +75,11 @@ server <- function(input, output) {
       setView(lng = 78, lat = 20, zoom = 2) %>%
       addTiles(urlTemplate = "") %>%
       setMaxBounds(lng1 = 180, lat1 = 84, lng2 = -140, lat2 = -84) %>%
-      #addProviderTiles("Stamen.TonerLite") %>% 
-      addPolygons(fillColor = ~ palette()(map_value), stroke = F, popup = ~shapeName, label = ~paste0(shapeName, ': ', ifelse(is.na(map_value), "No data", map_value)), highlight = highlightOptions(weight = 2, fillOpacity = 0.5, color = "black", opacity = 0.5, bringToFront = TRUE, sendToBack = TRUE))
+      addPolygons(fillColor = ~ palette()(map_value), stroke = F, popup = ~shapeName, label = ~paste0(shapeName, ': ', ifelse(is.na(map_value), "No data", map_value)), highlight = highlightOptions(weight = 2, fillOpacity = 0.5, color = "black", opacity = 0.5, bringToFront = TRUE, sendToBack = TRUE)) %>%
+      addLegend("topright", pal = palette(), values = ~map_value, title = "Value", opacity = 1)
   })
   output$Plots <- renderPlotly({
-    canvas <- ggplot(droplevels(manipulated_data[which(manipulated_data$administrative_area_level_1 == input$select_country), ]), aes(x = eval(parse(text = paste0("`", input$plot_x_axis, "`"))), y = eval(parse(text = paste0("`", input$plot_y_axis, "`"))))) +
+    canvas <- ggplot(droplevels(manipulated_data[which(manipulated_data$`Administrative Area Level 1` == input$select_country), ]), aes(x = eval(parse(text = paste0("`", input$plot_x_axis, "`"))), y = eval(parse(text = paste0("`", input$plot_y_axis, "`"))))) +
       coord_cartesian()
     `select colors` <- function(n) {
       hues <- seq(15, 375, length = n + 1)
@@ -94,7 +95,7 @@ server <- function(input, output) {
         )
       ),
       alpha = 0.75,
-      colour = colors[which(unique(manipulated_data$administrative_area_level_1) == input$select_country)]
+      colour = colors[which(unique(manipulated_data$`Administrative Area Level 1`) == input$select_country)]
       )) +
       labs(x = input$plot_x_axis, y = input$plot_y_axis) +
       theme_bw() +
