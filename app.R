2a81ce919e1be11496505ee7e9f0dbd5cf08bfd7
chrl3hr5 <chrl3hr5@gmail.com>
2025-05-28 18:00:02 +0530

    Made code changes for faster execution of application

--- app.R
+++ app.R
@@ -59,41 +59,35 @@ ui <- dashboardPage(
 # Server
 server <- function(input, output) {
   # Obtaining data to display on map based on the selected variable and time period
-  map_data <- reactive({req(input$time, input$information)
-    manipulated_data %>% filter(Date == input$time) %>% mutate(
+  map_data_merged <- reactive({req(input$time, input$information)
+    filtered_data <- manipulated_data %>% filter(Date == input$time) %>% mutate(
       map_value = as.numeric(.data[[input$information]]),
       shapeName = `Administrative Area Level 1`, .keep = "none")
+    values <- rep(NA, nrow(polygons))
+    indexes <- match(polygons$shapeName, filtered_data$shapeName)
+    values[!is.na(indexes)] <- filtered_data$map_value[indexes[!is.na(indexes)]]
+    polygons$map_value <- values
+    polygons
   })
-  # Merging the obtained data with polygon data
-  map_data_merged <- reactive({
-    req(map_data())
-    left_join(polygons, map_data(), by = "shapeName")
-  })
-  # Adding text to show as label
-  map_data_with_label <- reactive({
+  # Adding text to show as label and popup message
+  map_data_with_text <- reactive({
     req(map_data_merged())
-    map_data_with_label <- map_data_merged()
-    map_data_with_label$label <- paste0('<center> <p> <b>', map_data_with_label$shapeName, '</b> </br> (Click for info) </p> </center>')
-    map_data_with_label
-  })
-  # Adding text to show as popup message
-  map_data_with_popup <- reactive({
-    req(map_data_with_label())
-    map_data_with_popup <- map_data_with_label()
-    map_data_with_popup$popup <- paste0('<p> <b>', map_data_with_popup$shapeName, '</b>', '</br> <b> Date: </b>', input$time, '</br> <b>', input$information, ': </b>', ifelse(is.na(map_data_with_popup$map_value), "No data", map_data_with_popup$map_value),'</p>')
-    map_data_with_popup
+    map_data_with_text <- map_data_merged()
+    map_data_with_text$label <- paste0('<center> <p> <b>', map_data_with_text$shapeName, '</b> </br> (Click for info) </p> </center>')
+    map_data_with_text$popup <- paste0('<p> <b>', map_data_with_text$shapeName, '</b>', '</br> <b> Date: </b>', input$time, '</br> <b>', input$information, ': </b>', ifelse(is.na(map_data_with_text$map_value), "No data", map_data_with_text$map_value),'</p>')
+    map_data_with_text
   })
   # Creating palette based on country values for the selected variable
-  palette <- reactive({req(map_data_with_popup())
-    colorNumeric(palette = "viridis", domain = map_data_with_popup()$map_value, na.color = "#555555")
+  palette <- reactive({req(map_data_with_text())
+    colorNumeric(palette = "viridis", domain = map_data_with_text()$map_value, na.color = "#555555")
   })
   # Rendering map
   output$map <- renderLeaflet({
-    leaflet(map_data_with_popup(), options = leafletOptions(minZoom = 2)) %>%
+    leaflet(map_data_with_text(), options = leafletOptions(minZoom = 2)) %>%
       setView(lng = 78, lat = 20, zoom = 2) %>%
       addTiles(urlTemplate = "") %>%
       setMaxBounds(lng1 = 180, lat1 = 84, lng2 = -140, lat2 = -84) %>%
-      addPolygons(fillColor = ~ palette()(map_value), stroke = F, popup = lapply(map_data_with_popup()$popup, HTML), label = lapply(map_data_with_popup()$label, HTML), highlight = highlightOptions(weight = 2, fillOpacity = 0.5, color = "black", opacity = 0.5, bringToFront = TRUE, sendToBack = TRUE)) %>%
+      addPolygons(fillColor = ~ palette()(map_value), stroke = F, popup = lapply(map_data_with_text()$popup, HTML), label = lapply(map_data_with_text()$label, HTML), highlight = highlightOptions(weight = 2, fillOpacity = 0.5, color = "black", opacity = 0.5, bringToFront = TRUE, sendToBack = TRUE)) %>%
       addLegend("topright", pal = palette(), values = ~map_value, title = "Value", opacity = 1)
   })
   output$Plots <- renderPlotly({
