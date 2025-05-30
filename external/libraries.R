945f8a03c334b96fc4e4409579d916e44b445613
chrl3hr5 <chrl3hr5@gmail.com>
2025-05-30 14:38:43 +0530

    Changed libraries as per requirement

--- external/libraries.R
+++ external/libraries.R
@@ -1,7 +1,6 @@
 # Loading libraries
 library(tidyverse) # To perform data analysis
 library(plotly) # To create data visualization
-library(skimr) # For data summary
 library(COVID19) # To obtain COVID-19 data
 library(shiny) # For creating shiny application
 library(shinydashboard) # For creating shiny dashboard
@@ -11,3 +10,4 @@ library(httr) # To use API
 library(jsonlite) # To read JSON data
 library(sf) # To read geospatial data
 library(shinycssloaders) # To add loading animation
+library(rnaturalearth) # To get world polygons
